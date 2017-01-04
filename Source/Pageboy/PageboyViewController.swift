2d811fdba6f3c603533ef943ef1b9f8455733740
Merrick Sapsford <merrick@sapsford.tech>
2017-01-04 13:39:38 +0000

    Add basic pageboy view controller

---
+++ Source/Pageboy/PageboyViewController.swift
@@ -0,0 +1,65 @@
+//
+//  PageboyViewController.swift
+//  Pageboy
+//
+//  Created by Merrick Sapsford on 04/01/2017.
+//  Copyright © 2017 Merrick Sapsford. All rights reserved.
+//
+
+import UIKit
+
+class PageboyViewController: UIViewController {
+    
+    // MARK: Properties
+    
+    private var pageViewController: UIPageViewController?
+    
+    
+    public var navigationOrientation : UIPageViewControllerNavigationOrientation = .horizontal {
+        didSet {
+            self.setUpPageViewController()
+        }
+    }
+    
+    // MARK: Lifecycle
+    
+    override func loadView() {
+        super.loadView()
+        
+        self.setUpPageViewController()
+    }
+    
+    // MARK: Set Up
+    
+    private func setUpPageViewController() {
+        if self.pageViewController != nil { // destroy existing page VC
+            self.pageViewController?.view.removeFromSuperview()
+            self.pageViewController?.removeFromParentViewController()
+            self.pageViewController = nil
+        }
+        
+        let pageViewController = UIPageViewController(transitionStyle: .scroll,
+                                                      navigationOrientation: self.navigationOrientation,
+                                                      options: nil)
+        pageViewController.delegate = self
+        pageViewController.dataSource = self
+        self.pageViewController = pageViewController
+        
+        self.view.addSubview(pageViewController.view)
+        pageViewController.didMove(toParentViewController: self)
+    }
+}
+
+extension PageboyViewController: UIPageViewControllerDataSource, UIPageViewControllerDelegate {
+    
+    func pageViewController(_ pageViewController: UIPageViewController,
+                            viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        return nil
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController,
+                            viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        return nil
+    }
+    
+}
