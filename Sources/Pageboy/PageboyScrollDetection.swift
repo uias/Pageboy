86a4f6800a436639a2fb1a8c44accd0855b8ef02
Merrick Sapsford <merrick@sapsford.tech>
2017-02-28 10:34:29 +0000

    Remove logging

--- Sources/Pageboy/PageboyScrollDetection.swift
+++ Sources/Pageboy/PageboyScrollDetection.swift
@@ -96,9 +96,7 @@ extension PageboyViewController: UIPageViewControllerDelegate, UIScrollViewDeleg
         } else {
             positionPoint = CGPoint(x: scrollView.contentOffset.x, y: pagePosition)
         }
-        
-        print(positionPoint)
-        
+                
         // ignore duplicate updates
         guard self.currentPosition != positionPoint else { return }
         self.currentPosition = positionPoint
