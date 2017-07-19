95c6f819c029f2c74988a2bb9ab1248bd056c22e
Merrick Sapsford <merrick@sapsford.tech>
2017-07-19 10:49:32 +0100

    Fix test compilation errors

--- Sources/PageboyTests/TestComponents/TestPageboyDataSource.swift
+++ Sources/PageboyTests/TestComponents/TestPageboyDataSource.swift
@@ -9,31 +9,49 @@
 import Foundation
 import Pageboy
 
-class TestPageboyDataSource: Any, PageboyViewControllerDataSource {
+class TestPageboyDataSource: PageboyViewControllerDataSource {
     
-    var numberOfPages: Int?
-    var defaultIndex: PageboyViewController.PageIndex?
-    private(set) var viewControllers = [UIViewController]()
+    var numberOfPages: Int? {
+        didSet {
+            guard let numberOfPages = numberOfPages else {
+                self.viewControllers = nil
+                return
+            }
+            self.viewControllers = generateViewControllers(count: numberOfPages)
+        }
+    }
+    var defaultIndex: PageboyViewController.Page?
+    private(set) var viewControllers: [UIViewController]?
     
     private(set) var reloadCount = 0
     
-    func viewControllers(forPageboyViewController pageboyViewController: PageboyViewController) -> [UIViewController]? {
-        guard let numberOfPages = self.numberOfPages else {
-            return nil
-        }
+    // MARK: PageboyViewControllerDataSource
+    
+    func numberOfViewControllers(in pageboyViewController: PageboyViewController) -> Int {
+        return numberOfPages ?? 0
+    }
+    
+    func viewController(at index: PageboyViewController.PageIndex,
+                        in pageboyViewController: PageboyViewController) -> UIViewController? {
+        return self.viewControllers?[index]
+    }
+    
+    func defaultPage(for pageboyViewController: PageboyViewController) -> PageboyViewController.Page? {
+        return defaultIndex
+    }
+    
+    // MARK: Utility
+    
+    private func generateViewControllers(count: Int) -> [UIViewController] {
+        var viewControllers = [UIViewController]()
         
-        viewControllers.removeAll()
-        for index in 0..<numberOfPages {
+        for index in 0 ..< count {
+            
             let viewController = TestPageChildViewController()
             viewController.index = index
             viewControllers.append(viewController)
         }
         
-        reloadCount += 1
         return viewControllers
-    }
-    
-    func defaultPageIndex(forPageboyViewController pageboyViewController: PageboyViewController) -> PageboyViewController.PageIndex? {
-        return defaultIndex
     }
 }
