523c9c9ef65008627c468e61a4a1cadea2c5899f
Chris Stamper <christopherstamper@gmail.com>
2017-12-01 09:08:30 -0500

    Adjust PageViewController Scope
    The contained PageViewController may, in some cases, need to be accessible externally. In my example, I need to mask the edges of the scroll view for a particular layout. Since this is an edge case that's layout-dependent, doesn't really make sense to add a full masking capability to Pageboy; rather, the simple solution is to provide access to the underlying controller.

--- Sources/Pageboy/PageboyViewController.swift
+++ Sources/Pageboy/PageboyViewController.swift
@@ -46,7 +46,7 @@ open class PageboyViewController: UIViewController {
     
     // MARK: Properties
     
-    internal var pageViewController: UIPageViewController?
+    public var pageViewController: UIPageViewController?
     internal var previousPagePosition: CGFloat?
     internal var expectedTransitionIndex: PageIndex?
 
