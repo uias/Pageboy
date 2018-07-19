3f9463138f0e3e71c87dac49c94ddf3a043c99cc
Merrick Sapsford <merrick@sapsford.tech>
2018-07-19 19:23:49 +0200

    Add Settings button to example

---
+++ Example/Pageboy-Example/PageViewController+Settings.swift
@@ -0,0 +1,18 @@
+//
+//  PageViewController+Settings.swift
+//  Pageboy-Example
+//
+//  Created by Merrick Sapsford on 19/07/2018.
+//  Copyright © 2018 UI At Six. All rights reserved.
+//
+
+import UIKit
+
+extension PageViewController {
+    
+    // MARK: Actions
+    
+    @IBAction private func settingsButtonPressed(_ sender: UIButton) {
+        
+    }
+}
