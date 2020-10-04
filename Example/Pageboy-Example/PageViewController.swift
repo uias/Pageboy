5ac4c92219b5de1ccf10983528b04c47b7465269
Merrick Sapsford <merrick@sapsford.tech>
2020-10-04 12:35:36 +0200

    Remove BLTNBoard

--- Example/Pageboy-Example/PageViewController.swift
+++ Example/Pageboy-Example/PageViewController.swift
@@ -8,7 +8,6 @@
 
 import UIKit
 import Pageboy
-import BLTNBoard
 
 class PageViewController: PageboyViewController {
     
@@ -20,8 +19,6 @@ class PageViewController: PageboyViewController {
     }
     var previousBarButton: UIBarButtonItem?
     var nextBarButton: UIBarButtonItem?
-    
-    private var activeBulletinManager: BLTNItemManager?
     
     lazy var viewControllers: [UIViewController] = {
         var viewControllers = [UIViewController]()
@@ -47,12 +44,6 @@ class PageViewController: PageboyViewController {
         addBarButtonsIfNeeded()
     }
     
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        
-        showBulletin(makeIntroBulletinManager())
-    }
-    
     // MARK: Actions
     
     @objc func nextPage(_ sender: UIBarButtonItem) {
@@ -61,15 +52,6 @@ class PageViewController: PageboyViewController {
     
     @objc func previousPage(_ sender: UIBarButtonItem) {
         scrollToPage(.previous, animated: true)
-    }
-    
-    // MARK: Bulletins
-    
-    func showBulletin(_ manager: BLTNItemManager?) {
-        if let manager = manager {
-            self.activeBulletinManager = manager
-            manager.showBulletin(above: self)
-        }
     }
     
     // MARK: View Controllers
