24b5b812feacb1dc0b55c1e4a1055f7831e3eb73
msaps <merrick@sapsford.tech>
2022-11-03 12:20:33 +0100

    Fix page view controller crash during animation

--- Sources/Pageboy/Utilities/PatchedPageViewController.swift
+++ Sources/Pageboy/Utilities/PatchedPageViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 /// Fixes not updating dataSource on animated setViewControllers. See: https://stackoverflow.com/a/13253884/715593
 internal class PatchedPageViewController: UIPageViewController {
+
+    private var isAnimating = false
+
     override func setViewControllers(_ viewControllers: [UIViewController]?, direction: UIPageViewController.NavigationDirection, animated: Bool, completion: ((Bool) -> Void)? = nil) {
         super.setViewControllers(viewControllers, direction: direction, animated: animated) { (isFinished) in
-            if  isFinished && animated {
+            if isFinished && animated && !self.isAnimating {
+                self.isAnimating = true
                 DispatchQueue.main.async {
-                    super.setViewControllers(viewControllers, direction: direction, animated: false, completion: nil)
+                    super.setViewControllers(viewControllers, direction: direction, animated: false, completion: { _ in
+                        self.isAnimating = false
+                    })
                 }
             }
             
