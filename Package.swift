b3214ff5524b33dfea60d62d608c2f20062f2742
Merrick Sapsford <merrick@sapsford.tech>
2024-04-06 08:34:20 +0100

    Clean up Package.swift

--- Package.swift
+++ Package.swift
@@ -15,8 +15,6 @@ let package = Package(
             targets: ["Pageboy"])
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Pageboy",
             path: "Sources/Pageboy",
