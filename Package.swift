95ee98f9eba94c8f4a56d8b02da37da96f226f8f
Merrick Sapsford <merrick@sapsford.tech>
2024-04-06 09:43:00 +0200

    Add support for Privacy Manifest (#291)
    * Clean up Package.swift
    
    * Add PrivacyInfo.xcprivacy
    
    * Fix SPM platform support
    
    * Update Package to process PrivacyInfo
    
    * Add to podspec

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,8 +6,8 @@ import PackageDescription
 let package = Package(
     name: "Pageboy",
     platforms: [
-        .iOS(.v11),
-        .tvOS(.v11)
+        .iOS(.v12),
+        .tvOS(.v12)
     ],
     products: [
         .library(
@@ -15,12 +15,11 @@ let package = Package(
             targets: ["Pageboy"])
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Pageboy",
             path: "Sources/Pageboy",
-            exclude: ["Pageboy.h"]
+            exclude: ["Pageboy.h", "PrivacyInfo.xcprivacy"],
+            resources: [.process("PrivacyInfo.xcprivacy")]
         ),
         .testTarget(
             name: "PageboyTests",
